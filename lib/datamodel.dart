9e061d794ca7d3a07b4bc5fb0340a8993ef64adb
KingErrorcode <putipongpongpatcharapun@gmail.com>
2023-03-31 22:54:08 +0700

    edit

---
+++ lib/datamodel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'homescreen.dart';
+import 'main.dart';
+
+class Teamodel {
+  String drinkname;
+  String size;
+  double price;
+
+  Teamodel({
+    required this.drinkname,
+    required this.size,
+    required this.price,
+  });
+}
--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/datamodel.dart';
 import 'main.dart';
 
 void main() {
@@ -6,33 +7,39 @@ void main() {
 }
 
 class HomeScreen extends StatelessWidget {
+  //สร้างข้อมูลรายการหนังสือ
+  List<Teamodel> drinkDetail = [
+    Teamodel(drinkname: 'Thai Tea', size: 'S/M/L', price: 50/60/70),
+    Teamodel(drinkname: 'fruit Tea', size: 'S/M/L', price: 50/60/70),
+    Teamodel(drinkname: 'fruit Tea', size: 'S/M/L', price: 50/60/70),
+
+  ];
 @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Buddahist charm"),
+        title: const Text("___MENU___"),
         centerTitle: true,
-        backgroundColor: Colors.orange,
+        backgroundColor: Color.fromARGB(255, 211, 150, 60),
         elevation: 4,
       ),
-      body: SizedBox( 
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-          Icon(
-            Icons.sell,
-            size: 200,
-            color: Colors.black,
-          ),
-          Text(
-            "SELL",
-            style: TextStyle(
-              fontSize: 35,fontWeight: FontWeight.bold,color: Colors.red),
-            )
-          ],
-         ) 
-        ),
+      body: ListView.builder(
+        itemCount: drinkDetail.length,
+        itemBuilder: (context, index) {
+          var bookList = drinkDetail[index];
+          return ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(bookList.drinkname),
+                Text(bookList.price.toString()),
+              ],
+            ),
+            subtitle: Text(bookList.size),
+          );
+        },
+      ),
+      
     );
   }
 }
